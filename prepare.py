4cff8a501925f080f749770c4e4dd6082eec5ecf
Jason-Tellez <jason.michael.tellez@gmail.com>
2021-08-31 04:08:38 -0500

    Edits

--- prepare.py
+++ prepare.py
@@ -2,30 +2,13 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
-
-################### Get value counts for object columns ###################
-def check_v_counts(df):
-    '''
-    This function shows value counts for all columns by looping though each column, checking dtype, and shows appropriate stats
-    '''
-    x = []
-    y = []
-    for i in range(len(df.columns)):
-        if df[df.columns[i]].dtype == 'object':
-            print(f'{df.columns[i]}:\n{df[df.columns[i]].sort_values().value_counts()}\n')
-            print('----------\n')
-        elif df[df.columns[i]].dtype == 'int64':
-            print(f'{df.columns[i]}:\n{df[df.columns[i]].value_counts(bins=5, sort=False)}\n')
-            print('----------\n')
-            
             
 
 ################### Prepare dataframe by editing columns ###################
 def  prep_data(df):
     '''
     This function prepares and cleans the data by:
-        - adds '.00' to all total_charges values then cast column values as float dtype
+        - drops rows with empty total_charges values
         - replaces "Yes" or "No" values with 1 or 0, respectively
         - converts categorical vars to dummy vars then deletes first newly created dummy variable
             > combines dummy columns with original df
@@ -33,9 +16,11 @@ def  prep_data(df):
             > combines dummy columns with original df
         - drops any existing duplicate rows
         - drops unsuable or unnecessary columns
+        - creates new column 'tenure_years'
     '''
     # replace empty spaces with 0.00 and convert total_charges to float dtype
-    df['total_charges'] = df.total_charges.replace(' ', '0.00').astype(float)
+    df.drop(index=df[df.total_charges==' '].index.values.tolist(), inplace=True)
+    df['total_charges'] = df.total_charges.astype(float)
     
     # replace yes/no with 1/ 0
     yes_no_cols = ['paperless_billing', 'partner', 'dependents', 'phone_service', 'churn']
@@ -57,33 +42,27 @@ def  prep_data(df):
     # drops unusable or unecessary columns and columns with new dummy vars
     df.drop(columns=['customer_id', 'gender', 'contract_type','payment_type', 'internet_service_type', 'contract_type_Two year'], inplace=True)
     
+    # adds tenure years column
+    df['tenure_years'] = round(df.tenure / 12, 1)
+    
     return df
 
 
 
-###################  ###################
-
-
-
-
-###################  ###################
-
-
-
-
-###################  ###################
-
-
-
-
-###################  ###################
-
-
-
-
-###################  ###################
-
-
-
-
-###################  ###################
+################### Train, validate, and test ###################
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df.churn)
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate.churn)
+    return train, validate, test
